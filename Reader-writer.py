27401e167e689acb733a4b5cfa4f09ea60b5cea5
nitrohub <nitesh.h@somaiya.edu>
2018-10-23 09:55:32 +0530

    Reader-writer with 2 readers

--- Reader-writer.py
+++ Reader-writer.py
@@ -5,7 +5,8 @@ s = 1  # semaphore for reader and writer as in producer consumer
 mutex = 1  # when the reader is incrementing read count
 readcount = 0  # To keep a track of all the readers entered
 count = 0
-i=0 #For iterating through the count 
+i=0 #For iterating through the count for reader1
+k=0 #For iterating through the count for reader2
 
 
 def wait(para):
@@ -13,11 +14,11 @@ def wait(para):
     global mutex
     global readcount
     global count
-    print(para + " is locked!")
     while s <= 0:
         print("Blocked!!")
-        sleep(2)
+        sleep(0.5)
     s = s - 1
+    print(para + " is locked!")
 
 
 def signal(para):
@@ -25,8 +26,8 @@ def signal(para):
     global mutex
     global readcount
     global count
-    print(para + " is released!")
     s = s + 1
+    print(para + " is released!")
 
 
 def mwait():
@@ -81,10 +82,11 @@ class reader1(Thread):
         	if readcount == 1:
         	    wait("writer")  # locking the writer
         	msignal()
-	
-        	for j in range(i, count):
+        	print("Number of reader in critical section:"+str(readcount))
+        	for j in range(i, count): #Critical section
         	    print("Reader 1 reading block" + str(j))
         	    i=i+1
+        	    sleep(2)
 
         	mwait()
         	readcount = readcount - 1
@@ -93,39 +95,46 @@ class reader1(Thread):
         	msignal()
         	sleep(3)
 
+class reader2(Thread):
+    def run(self):
+        global s
+        global mutex
+        global readcount
+        global count
+        global k
+        while k<count:
+        	print("\nReader2 trying to Read!")
+        	# sleep(1)
+        	mwait()  # stopping other readers until readcount is being altered
+        	readcount = readcount + 1
+        	if readcount == 1:
+        	    wait("writer")  # locking the writer
+        	msignal()
+        	print("Number of reader in critical section:"+str(readcount))
+        	for j in range(k, count):
+        	    print("Reader 2 reading block" + str(j))
+        	    k=k+1
+        	    sleep(2)
+
+        	mwait()
+        	readcount = readcount - 1
+        	if readcount == 0:
+        		signal("writer")  # Releasing the writer
+        	msignal()
+        	sleep(3)
 
         
 
 
-# class reader2(Thread):
-#     def run(self):
-#         global s
-#         global mutex
-#         global readcount
-#         global count
-#         print("Reader2 started reading")
-#         mwait()  # stopping other readers until readcount is being altered
-#         readcount += 1
-#         if readcount == 1:
-#             wait("writer")  # locking the writer
-#         msignal()
 
-#         for i in range(0, 5):
-#             print("Reader 2 reading block" + str(i))
-
-#         mwait()
-#         readcount -= 1
-#         if readcount == 0:
-#             signal("writer")  # Releasing the writer
-#         msignal()
 
 
 if __name__ == "__main__":
-    w = writer().start()
-    sleep(2)
-    r1 = reader1().start()
-# sleep(2)
-# r2=reader2().start()
+	w = writer().start()
+	sleep(1)
+	r1 = reader1().start()
+	sleep(1)
+	r2=reader2().start()
 
 
 
